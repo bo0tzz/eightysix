cdc02064b0fa54bf3e10c82e9a6d006f2a1562f5
Boet de Willigen <boet.dewilligen@securew2.com>
2020-12-09 16:06:43 +0100

    Set up config loading

--- lib/eightysix/application.ex
+++ lib/eightysix/application.ex
@@ -3,13 +3,28 @@ defmodule Eightysix.Application do
   # for more information on OTP Applications
   @moduledoc false
 
+  @config_bindings [
+    bot_token: ["bot", "token"]
+  ]
+
   use Application
 
   @impl true
   def start(_type, _args) do
+    config_file = case System.fetch_env("EIGHTYSIX_CONFIG_FILE") do
+      :error -> "config.toml"
+      {:ok, value} -> value
+    end
+
+    providers = [
+      %Vapor.Provider.File{path: config_file, bindings: @config_bindings}
+    ]
+
+    config = Vapor.load!(providers) |> IO.inspect()
+
     children = [
       ExGram,
-      {Eightysix.Bot, [method: :polling, token: token]}
+      {Eightysix.Bot, [method: :polling, token: config.bot_token]}
     ]
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
