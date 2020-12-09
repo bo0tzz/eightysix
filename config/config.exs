4dbfd161db351140ebf0b06a77ae5397ad169d99
Boet de Willigen <boet.dewilligen@securew2.com>
2020-12-09 22:54:45 +0100

    The reminder for the bins should be sent on Tuesdays

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ import Config
 
 config :eightysix, Eightysix.Scheduler,
   jobs: [
-    {"0 15 * * 3", {Eightysix.Scheduler, :send_bins_reminder, []}}
+    {"0 15 * * 2", {Eightysix.Scheduler, :send_bins_reminder, []}}
   ]
